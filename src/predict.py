e9b01b25dbf269934345b11ebe7f6f32ed2412ab
dipesh-2005 <dipesh.bhattarai.dipu@gmail.com>
2026-07-17 15:10:39 +0545

    updated predict.py

--- src/predict.py
+++ src/predict.py
@@ -35,6 +35,14 @@ def predict_match(home_team, away_team, tournament, is_neutral):
 
     winner = label_encoder.inverse_transform([winner_encoded])[0]
 
+# Convert label to actual team name
+    if winner == "Home":
+      winner = home_team
+    elif winner == "Away":
+      winner = away_team
+    else:
+      winner = "Draw"
+
     # -----------------------------------
     # Winner probabilities
     # -----------------------------------
@@ -44,11 +52,17 @@ def predict_match(home_team, away_team, tournament, is_neutral):
         list(range(len(probs)))
     )
 
-    probabilities = {
-        cls: round(float(prob), 3)
-        for cls, prob in zip(classes, probs)
-    }
+    probabilities = {}
 
+    for cls, prob in zip(classes, probs):
+        if cls == "Home":
+            probabilities[home_team] = round(float(prob), 3)
+        elif cls == "Away":
+            probabilities[away_team] = round(float(prob), 3)
+        else:
+            probabilities["Draw"] = round(float(prob), 3)
+    # Maximum win probability
+    max_probability = max(probabilities.values())
     # -----------------------------------
     # Goal prediction
     # -----------------------------------
@@ -60,14 +74,59 @@ def predict_match(home_team, away_team, tournament, is_neutral):
         float(away_model.predict(X_goals)[0])
     )
 
+
+    # -----------------------------------
+# Make score consistent with winner
+# -----------------------------------
+
+    if winner == home_team:
+
+        if home_goals <= away_goals:
+            home_goals = away_goals + 1
+
+    # Increase goals if confidence is high
+        if max_probability >= 0.95:
+            home_goals = max(home_goals, 4)
+
+        elif max_probability >= 0.85:
+            home_goals = max(home_goals, 3)
+
+        elif max_probability >= 0.70:
+            home_goals = max(home_goals, 2)
+
+
+    elif winner == away_team:
+
+        if away_goals <= home_goals:
+            away_goals = home_goals + 1
+
+        if max_probability >= 0.95:
+            away_goals = max(away_goals, 4)
+
+        elif max_probability >= 0.85:
+            away_goals = max(away_goals, 3)
+
+        elif max_probability >= 0.70:
+            away_goals = max(away_goals, 2)
+
+
+    else:
+
+        if home_goals > away_goals:
+            away_goals = home_goals
+
+        elif away_goals > home_goals:
+            home_goals = away_goals
+    
+
     # -----------------------------------
     # Return result
     # -----------------------------------
     return {
-        "home_team": home_team,
-        "away_team": away_team,
-        "winner": winner,
-        "home_goals": home_goals,
-        "away_goals": away_goals,
-        "probabilities": probabilities
-    }
+    "match": f"{home_team} vs {away_team}",
+    "winner": winner,
+    "score": f"{home_team} {home_goals} - {away_goals} {away_team}",
+    "home_goals": home_goals,
+    "away_goals": away_goals,
+    "probabilities": probabilities
+}
